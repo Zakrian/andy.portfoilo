f6b648f4785b65686bc2a5fe00e4d6c82379c451
Zakrian <82257242+Zakrian@users.noreply.github.com>
2022-10-05 18:50:11 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,80 +1,46 @@
 // Переменные и константы ====================================
 //Burger menu
-const menuBurger = document.querySelector('.menu-icon');
-const menuBody = document.querySelector('.menu');
+const menuBurger = document.querySelector('.menu-icon'),
+	menuBody = document.querySelector('.menu'),
+	//Modal window
+	introBtn = document.querySelector('.intro__btn'),
+	modalBtn = document.querySelector('.modal__btn'),
+	modal = document.querySelector('.modal'),
+	modalWrap = document.querySelector('modal__wrap'),
+	body = document.querySelector('body'),
+	overlay = document.querySelector('.overlay');
 //DOM Moving
-let main = document.querySelector('.main')
-let brends = document.querySelector('.brends')
-//Modal window
-const introBtn = document.querySelector('.intro__btn')
-const modalBtn = document.querySelector('.modal__btn')
-const modal = document.querySelector('.modal')
-const modalWrap = document.querySelector('modal__wrap')
-const body = document.querySelector('body')
-const overlay = document.querySelector('.overlay')
+let main = document.querySelector('.main'),
+	brends = document.querySelector('.brends');
 const removeShowClass = () => {
-	modal.classList.remove('_show-modal')
-	body.classList.remove('body_fixed')
-	overlay.classList.remove('_show-overlay')
-}
+	modal.classList.remove('_show-modal');
+	body.classList.remove('body_fixed');
+	overlay.classList.remove('_show-overlay');
+};
 
 // Modal window =============================================================
 introBtn.addEventListener('click', function (e) {
 	// e.preventDefault
-	modal.classList.add('_show-modal')
-	body.classList.add('body_fixed')
-	overlay.classList.add('_show-overlay')
-})
+	modal.classList.add('_show-modal');
+	body.classList.add('body_fixed');
+	overlay.classList.add('_show-overlay');
+});
 
 modalBtn.addEventListener('click', function () {
-	removeShowClass()
-})
+	removeShowClass();
+});
 overlay.addEventListener('click', function () {
-	removeShowClass()
-})
-
-// Определение типа устройства ===============================
-// const isMobile = {
-// 	Android: function () {
-// 		return navigator.userAgent.match(/Android/i);
-// 	},
-// 	BlackBerry: function () {
-// 		return navigator.userAgent.match(/BlackBerry/i);
-// 	},
-// 	iOS: function () {
-// 		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-// 	},
-// 	Opera: function () {
-// 		return navigator.userAgent.match(/Opera Mini/i);
-// 	},
-// 	Windows: function () {
-// 		return navigator.userAgent.match(/IEMobile/i);
-// 	},
-// 	any: function () {
-// 		return (
-// 			isMobile.Android() ||
-// 			isMobile.BlackBerry() ||
-// 			isMobile.iOS() ||
-// 			isMobile.Opera() ||
-// 			isMobile.Windows());
-// 	}
-// };
+	removeShowClass();
+});
 
 // Меню бургер ===============================================
-
-//Логика скрипта бургера:
-//1. Получаем объект бургер
-//2. Получаем меню, которое нужно открыть
-//3. Вешаем обработчик события click на бургер (п.1)
-//4. Добавляем (toggle) класс к меню (п.2)
-
 if (menuBurger) {
 	menuBurger.addEventListener("click", function (e) {
 		menuBurger.classList.toggle('_active');
 		if (menuBody) {
 			menuBody.classList.toggle('_active');
 		}
-	})
+	});
 }
 
 // Плавный скролл к якорю ===================================
@@ -87,8 +53,7 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
 
 		const scrollTarget = document.getElementById(href);
 
-		// const topOffset = document.querySelector('.scrollto').offsetHeight;
-		const topOffset = 80; // Отступ сверху 
+		const topOffset = 80;
 		const elementPosition = scrollTarget.getBoundingClientRect().top;
 		const offsetPosition = elementPosition - topOffset;
 
@@ -101,79 +66,8 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
 
 // Изменение порядков элементов DOM при определенной ширине экрана ================
 if (window.innerWidth <= 767.99) {
-	main.append(brends)
+	main.append(brends);
 }
-
-// Аккордеон ======================================================================
-// const questions = document.querySelectorAll('.accordion');
-
-// const closeQuestion = (el) => {
-// 	const content = el.querySelector('.accordion__text');
-// 	el.classList.remove('_active-acc');
-// 	content.style.maxHeight = null;
-// }
-
-// const openQuestion = (el) => {
-// 	const content = el.querySelector('.accordion__text');
-// 	el.classList.add('_active-acc');
-// 	content.style.maxHeight = content.scrollHeight + 'px';
-// }
-
-// const closeAllQuestions = () => questions.forEach(question => closeQuestion(question));
-
-// questions.forEach((el) => {
-// 	el.addEventListener('click', () => {
-// 		if (el.classList.contains('_active-acc')) {
-// 			closeQuestion(el);
-// 		} else {
-// 			closeAllQuestions();
-// 			openQuestion(el);
-// 		}
-// 	});
-// })
-
-// Animation on scroll ============================================================
-// const animItems = document.querySelectorAll('._anim-items');
-
-// if (animItems.length > 0) {
-// 	window.addEventListener('scroll', animOnScroll);
-
-// 	function animOnScroll() {
-// 		for (let index = 0; index < animItems.length; index++) {
-// 			const animItem = animItems[index];
-// 			const animItemHeight = animItem.offsetHeight;
-// 			const animItemOffset = offset(animItem).top;
-// 			const animStart = 4;
-
-// 			let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-// 			if (animItemHeight > window.innerHeight) {
-// 				let animItemPoint = window.innerHeight - window.innerHeight / animStart;
-// 			}
-
-// 			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-// 				animItem.classList.add('_show');
-// 			} else {
-// 				if (!animItem.classList.contains('_anim-no-hide')) {
-// 					animItem.classList.remove('_show');
-// 				}
-// 			}
-// 		}
-// 	}
-
-// 	function offset(el) {
-// 		const rect = el.getBoundingClientRect(),
-// 			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-// 			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-// 		return {
-// 			top: rect.top + scrollTop,
-// 			left: rect.left + scrollLeft
-// 		}
-// 	}
-// 	setTimeout(() => {
-// 		animOnScroll();
-// 	}, 500);
-// }
 
 // Swiper slider ============================================================
 const swiper = new Swiper('.swiper', {
@@ -195,3 +89,67 @@ const swiper = new Swiper('.swiper', {
 		delay: 800,
 	},
 });
+
+// Class for services section & his instances======================================
+class ServicesCard {
+	constructor(link, src, alt, title, descr, parentSelector) {
+		this.link = link;
+		this.src = src;
+		this.alt = alt;
+		this.title = title;
+		this.descr = descr;
+		this.parent = document.querySelector(parentSelector);
+	}
+
+	render() {
+		const el = document.createElement('div');
+		el.classList.add('services__item');
+
+		el.innerHTML = `
+			<a href=${this.link} target="_blank" class="services__link btn__pc"></a>
+			<div class="services__img">
+				<img src=${this.src} alt=${this.alt}>
+			</div>
+			<div class="services__name">${this.title}</div>
+			<div class="services__descr">${this.descr}</div>
+		`;
+
+		this.parent.append(el);
+	}
+}
+
+new ServicesCard(
+	'https://zakrian.github.io/coolerwash/',
+	'img/works/CoolerWash.png',
+	'Porten',
+	'Обслуживание сплит систем',
+	'Профилактическая (плановая) чистка внутреннего и внешнего блоков сплит системы водой под давлением, а также чистка пластикового корпуса внутреннего блока.',
+	'.services__inner'
+).render();
+
+new ServicesCard(
+	'https://zakrian.github.io/cootels/',
+	'img/works/Cootels.png',
+	'Cootels',
+	'Бронирование комнат и отелей',
+	'Комплексная чистка внутреннего и внешнего блоков сплит системы водой под давлением, антибактериальная обработка испарителя и вала(крыльчатки), а также чистка пластикового корпуса внутреннего блока',
+	'.services__inner'
+).render();
+
+new ServicesCard(
+	'https://zakrian.github.io/denisnovik/',
+	'img/works/DenisNovik.png',
+	'Denis Novik',
+	'Сайт портфолио UI/UX дизайнера',
+	'Профилактическая (плановая) чистка внутреннего и внешнего блоков сплит системы водой под давлением, а также чистка пластикового корпуса внутреннего блока.',
+	'.services__inner'
+).render();
+
+new ServicesCard(
+	'https://zakrian.github.io/porten/',
+	'img/works/Porten.png',
+	'Porten',
+	'Интернет-магазин мужских часов',
+	'Профилактическая (плановая) чистка внутреннего и внешнего блоков сплит системы водой под давлением, а также чистка пластикового корпуса внутреннего блока.',
+	'.services__inner'
+).render();
